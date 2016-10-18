de08347b77b783c6c316fe6dc50ef6b8dd08dbb7
Rosca <sabina.rosca@wur.nl>
2016-10-18 17:07:50 +0200

    Fist commit: created project and implemented version control

---
+++ main.R
@@ -0,0 +1,17 @@
+#Sabina Rosca
+#October 2016
+#Testing bfast Spatial with different settings
+#in order to provide support for FAO
+
+#Install packages
+install.packages("raster")
+install.packages("sp")
+install.packages("gdal")
+
+#Import packages
+library(sp)
+library(raster)
+library(gdal)
+
+#Source functions
+source("R/")
